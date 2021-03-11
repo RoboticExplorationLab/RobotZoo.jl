fde1c1b9d29b0434d5b9985567cfb0e02c0cc49a
Brian Jackson <bjack205@gmail.com>
2021-03-11 09:19:29 -0500

    Cartpole keyword constructor

--- src/cartpole.jl
+++ src/cartpole.jl
@@ -20,7 +20,7 @@ struct Cartpole{T} <: AbstractModel
     g::T
 end
 
-Cartpole() = Cartpole(1.0, 0.2, 0.5, 9.81)
+Cartpole(; mc=1.0, mp=0.2, l=0.5, g=9,81) = Cartpole(mc, mp, l, g)
 
 function dynamics(model::Cartpole, x, u)
     mc = model.mc  # mass of the cart in kg (10)
