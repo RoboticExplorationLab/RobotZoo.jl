2b866ad918e8a0833bd03dc09a6f918949c1a9bb
Brian Jackson <bjack205@gmail.com>
2022-01-16 13:51:12 -0500

    Fix tolerances

--- test/models_test.jl
+++ test/models_test.jl
@@ -1,97 +1,86 @@
 const RD = RobotDynamics
 using RobotDynamics: KnotPoint, dynamics, dynamics!, jacobian!
 using RobotDynamics: StaticReturn, InPlace, ForwardAD, FiniteDifference
-function num_allocs(model; evals=1, samples=1, discrete=true) 
+function test_model(model; evals=1, samples=1, tol=1e-6) 
     println(typeof(model))
     dmodel = RD.DiscretizedDynamics{RD.RK4}(model)
     t,dt = 1.1,0.1
     x, u = rand(model)
     n,m = RD.dims(model)
     z = KnotPoint(x, u, t, dt)
-    ∇c  = zeros(n,n+m)
+    ∇c1  = zeros(n,n+m)
+    ∇c2  = zeros(n,n+m)
     xdot = zeros(n)
     allocs = 0
     allocs += @ballocated RobotDynamics.dynamics($model, $x, $u) evals=evals samples=samples
     allocs += @ballocated RobotDynamics.dynamics!($model, $xdot, $x, $u) evals=evals samples=samples
     @test xdot == RobotDynamics.dynamics(model, x, u)
     @test allocs == 0 
-    allocs += @ballocated RobotDynamics.jacobian!(StaticReturn(), ForwardAD(), $model, $∇c, $xdot, $z) evals=evals samples=samples
-    allocs += @ballocated RobotDynamics.jacobian!(StaticReturn(), FiniteDifference(), $model, $∇c, $xdot, $z) evals=evals samples=samples
-    @test allocs == 0 
-    allocs += @ballocated RobotDynamics.jacobian!(InPlace(), ForwardAD(), $model, $∇c, $xdot, $z) evals=evals samples=samples
-    allocs += @ballocated RobotDynamics.jacobian!(InPlace(), FiniteDifference(), $model, $∇c, $xdot, $z) evals=evals samples=samples
-    @test allocs == 0 
-
-    # allocs = 0
-    # if discrete 
-    #     allocs += @ballocated RobotDynamics.discrete_dynamics($dmodel, $z) evals=evals samples=samples
-    #     allocs += @ballocated RobotDynamics.discrete_dynamics!($dmodel, $xdot, $z) evals=evals samples=samples
-    #     allocs += @ballocated RobotDynamics.jacobian!(StaticReturn(), ForwardAD(), $dmodel, $∇c, $xdot, $z) evals=evals samples=samples
-    #     allocs += @ballocated RobotDynamics.jacobian!(StaticReturn(), FiniteDifference(), $dmodel, $∇c, $xdot, $z) evals=evals samples=samples
-    #     allocs += @ballocated RobotDynamics.jacobian!(InPlace(), ForwardAD(), $dmodel, $∇c, $xdot, $z) evals=evals samples=samples
-    #     allocs += @ballocated RobotDynamics.jacobian!(InPlace(), FiniteDifference(), $dmodel, $∇c, $xdot, $z) evals=evals samples=samples
-    #     @test allocs == 0 
-    # end
-
-    return allocs
+    RobotDynamics.jacobian!(StaticReturn(), ForwardAD(), model, ∇c1, xdot, z)
+    RobotDynamics.jacobian!(StaticReturn(), FiniteDifference(), model, ∇c2, xdot, z)
+    @test ∇c1 ≈ ∇c2 atol=tol
+    RobotDynamics.jacobian!(InPlace(), ForwardAD(), model, ∇c2, xdot, z)
+    @test ∇c1 ≈ ∇c2
+    RobotDynamics.jacobian!(InPlace(), FiniteDifference(), model, ∇c1, xdot, z)
+    @test ∇c1 ≈ ∇c2 atol=tol
 end
 
 # Acrobot
 acrobot = RobotZoo.Acrobot()
 @test RD.dims(acrobot) == (4,1,4)
-@test num_allocs(acrobot) == 0
+test_model(acrobot)
 
 # Car
 car = RobotZoo.DubinsCar()
 @test RD.dims(car) == (3,2,3)
-@test num_allocs(car) == 0
+test_model(car)
 
 # Bicycle Car
 bicycle = RobotZoo.BicycleModel()
 @test RD.dims(bicycle) == (4,2,4)
-@test num_allocs(bicycle) == 0
+test_model(bicycle)
 
 bicycle = RobotZoo.BicycleModel(ref=:rear)
 @test RD.dims(bicycle) == (4,2,4)
-@test num_allocs(bicycle) == 0
+test_model(bicycle)
 
 # Planar Rocket
 rocket = RobotZoo.PlanarRocket()
 @test RD.dims(rocket) == (8,2,8)
-@test num_allocs(rocket) == 0
+test_model(rocket)
 
 # Planar Quad
 quad = RobotZoo.PlanarQuadrotor()
 @test RD.dims(quad) == (6,2,6)
-@test num_allocs(rocket) == 0
+test_model(quad)
 
 # Cartpole
 cartpole = RobotZoo.Cartpole()
 @test RD.dims(cartpole) == (4,1,4)
-@test num_allocs(cartpole) == 0
+test_model(cartpole)
 
 # Double Integrator
 dim = 3
 di = RobotZoo.DoubleIntegrator(dim)
 n,m = size(di)
 @test (n,m) == (6,3)
-@test num_allocs(di) == 0
+test_model(di)
 
 # Pendulum
 pend = RobotZoo.Pendulum()
 RobotZoo.Pendulum{Float64}(1,1,1, 1,1,1)
 @test RD.dims(pend) == (2,1,2)
-@test num_allocs(pend) == 0
+test_model(pend)
 
 # Quadrotor
 quad = RobotZoo.Quadrotor()
 @test RD.dims(quad) == (13,4,13)
-@test num_allocs(quad, discrete=false) == 0
+test_model(quad,tol=1e-4)
 
 # Yak Plane
 yak = RobotZoo.YakPlane(MRP{Float64})
 @test RD.dims(yak) == (12,4,12)
-@test num_allocs(yak, discrete=false) == 0
+test_model(yak,tol=1e-4)
 
 # Test other functions
 dt = 0.1
