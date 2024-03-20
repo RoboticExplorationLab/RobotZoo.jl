0819926ab69a409c61dfb77731b7f15ed82fa21f
Brian Jackson <brian@albedo.com>
2024-03-20 15:46:51 -0400

    Update formatting

--- src/linearmodel.jl
+++ src/linearmodel.jl
@@ -11,29 +11,29 @@ struct LinearModel{M,V} <: RD.ContinuousDynamics
     A::M
     B::M
     d::V
-    function LinearModel(A::M, B::M, d::V) where {M<:AbstractMatrix, V<:AbstractVector}
-        if !(size(A,1) == size(B,1) == size(d,1))
+    function LinearModel(A::M, B::M, d::V) where {M<:AbstractMatrix,V<:AbstractVector}
+        if !(size(A, 1) == size(B, 1) == size(d, 1))
             throw(DimensionMismatch("All inputs must have the same number of rows."))
-        elseif size(d,2) != 1
+        elseif size(d, 2) != 1
             throw(DimensionMismatch("d must have a single column."))
         end
-        new{M,V}(A,B,d)
+        new{M,V}(A, B, d)
     end
 end
 
 function LinearModel(A::AbstractMatrix, B::AbstractMatrix)
-    d = similar(A, size(A,1))
+    d = similar(A, size(A, 1))
     d .= 0
     LinearModel(A, B, d)
 end
 
-function LinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod, 
-                     model::RD.ContinuousDynamics, x, u, t=zero(eltype(x)); affine::Bool=true)
-    n,m = RD.dims(model)
-    J = zeros(n,n+m)
+function LinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod,
+    model::RD.ContinuousDynamics, x, u, t=zero(eltype(x)); affine::Bool=true)
+    n, m = RD.dims(model)
+    J = zeros(n, n + m)
     d = zeros(n)
     z = RD.KnotPoint{n,m}(x, u, t, NaN)
-    RD.jacobian!(sig, diffmethod, model, J, d, z) 
+    RD.jacobian!(sig, diffmethod, model, J, d, z)
     if affine
         if sig == RD.InPlace()
             RD.dynamics!(model, d, x, u, t)
@@ -43,14 +43,14 @@ function LinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod,
     else
         d .= 0
     end
-    A = J[:,1:n]
-    B = J[:,n+1:n+m]
+    A = J[:, 1:n]
+    B = J[:, n+1:n+m]
     LinearModel(A, B, d)
 end
 
 # Generates a random controllable linear system
 function Base.rand(::Type{LinearModel}, n::Integer, m::Integer)
-    A,B = RandomLinearModels.gencontrollable(n, m, :continuous)
+    A, B = RandomLinearModels.gencontrollable(n, m, :continuous)
     d = randn(n)
     LinearModel(A, B, d)
 end
@@ -85,9 +85,9 @@ function dynamics!(model::LinearModel, xn, x, u)
 end
 
 function jacobian!(model::LinearModel, J, xn, x, u)
-    n,m = RD.dims(model)
-    J[:,1:n] .= model.A
-    J[:,n+1:n+m] .= model.B
+    n, m = RD.dims(model)
+    J[:, 1:n] .= model.A
+    J[:, n+1:n+m] .= model.B
     nothing
 end
 
@@ -109,12 +109,12 @@ struct DiscreteLinearModel{M,V} <: RD.DiscreteDynamics
     CA::M
     CB::M
     Cd::V
-    function DiscreteLinearModel(dt, A::M, B::M, C::M, d::V) where {M<:AbstractMatrix, V<:AbstractVector}
-        if !(size(A,1) == size(B,1) == size(C,1) == size(d,1))
+    function DiscreteLinearModel(dt, A::M, B::M, C::M, d::V) where {M<:AbstractMatrix,V<:AbstractVector}
+        if !(size(A, 1) == size(B, 1) == size(C, 1) == size(d, 1))
             throw(DimensionMismatch("All inputs must have the same number of rows."))
-        elseif size(A,2) != size(C,2)
+        elseif size(A, 2) != size(C, 2)
             throw(DimensionMismatch("A and C must have the same number of columns."))
-        elseif size(d,2) != 1
+        elseif size(d, 2) != 1
             throw(DimensionMismatch("d must have a single column."))
         end
         if C isa SMatrix
@@ -122,30 +122,30 @@ struct DiscreteLinearModel{M,V} <: RD.DiscreteDynamics
         else
             F = factorize(C)
         end
-        CA = -(F\A)
-        CB = -(F\B)
-        Cd = -(F\d)
-        new{M,V}(A,B,C,d,dt, CA,CB,Cd)
+        CA = -(F \ A)
+        CB = -(F \ B)
+        Cd = -(F \ d)
+        new{M,V}(A, B, C, d, dt, CA, CB, Cd)
     end
 end
 
-function DiscreteLinearModel(dt, A::AbstractMatrix, B::AbstractMatrix, 
-                             d::AbstractVector=(similar(A, size(A,1)) .= zero(eltype(A))))
+function DiscreteLinearModel(dt, A::AbstractMatrix, B::AbstractMatrix,
+    d::AbstractVector=(similar(A, size(A, 1)) .= zero(eltype(A))))
     C = similar(A) * zero(eltype(A))
-    for i = 1:size(C,1)
-        C[i,i] = -one(eltype(A))
+    for i = 1:size(C, 1)
+        C[i, i] = -one(eltype(A))
     end
     DiscreteLinearModel(dt, A, B, C, d)
 end
 
-function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod, 
-                             model::RD.DiscretizedDynamics, 
-                             x, u, t, h; affine::Bool=true)
-    n,m = RD.dims(model)
-    J = zeros(n,n+m)
+function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod,
+    model::RD.DiscretizedDynamics,
+    x, u, t, h; affine::Bool=true)
+    n, m = RD.dims(model)
+    J = zeros(n, n + m)
     d = zeros(n)
-    z = RD.KnotPoint{n,m}([x;u], t, h)
-    RD.jacobian!(sig, diffmethod, model, J, d, z) 
+    z = RD.KnotPoint{n,m}([x; u], t, h)
+    RD.jacobian!(sig, diffmethod, model, J, d, z)
     if affine
         if sig == RD.InPlace()
             RD.discrete_dynamics!(model, d, x, u, t, h)
@@ -155,22 +155,22 @@ function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMetho
     else
         d .= 0
     end
-    A = J[:,1:n]
-    B = J[:,n+1:n+m]
+    A = J[:, 1:n]
+    B = J[:, n+1:n+m]
     DiscreteLinearModel(h, A, B, d)
 end
 
-function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod, 
-                             model::RD.ImplicitDynamicsModel, 
-                             x, u, t, h; affine::Bool=true)
-    n,m = RD.dims(model)
-    J1 = zeros(n,n+m)
-    J2 = zeros(n,n+m)
+function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMethod,
+    model::RD.ImplicitDynamicsModel,
+    x, u, t, h; affine::Bool=true)
+    n, m = RD.dims(model)
+    J1 = zeros(n, n + m)
+    J2 = zeros(n, n + m)
     d = zeros(n)
-    y1 = similar(d) 
-    z1 = RD.StaticKnotPoint{Any,Any}(n,m, [x;u], t, h)
-    z2 = RD.StaticKnotPoint{Any,Any}(n,m, [x;u], t, h)
-    RD.dynamics_error_jacobian!(sig, diffmethod, model, J2, J1, d, y1, z2, z1) 
+    y1 = similar(d)
+    z1 = RD.StaticKnotPoint{Any,Any}(n, m, [x; u], t, h)
+    z2 = RD.StaticKnotPoint{Any,Any}(n, m, [x; u], t, h)
+    RD.dynamics_error_jacobian!(sig, diffmethod, model, J2, J1, d, y1, z2, z1)
     if affine
         if sig == RD.InPlace()
             RD.dynamics_error!(model, d, y1, z2, z1)
@@ -180,10 +180,10 @@ function DiscreteLinearModel(sig::RD.FunctionSignature, diffmethod::RD.DiffMetho
     else
         d .= 0
     end
-    A = J1[:,1:n]
-    B = J1[:,n+1:n+m]
-    C = J2[:,1:n]
-    if norm(J2[:,n+1:n+m]) > 0
+    A = J1[:, 1:n]
+    B = J1[:, n+1:n+m]
+    C = J2[:, 1:n]
+    if norm(J2[:, n+1:n+m]) > 0
         error("Cannot form a DiscreteLinearModel from an implicit dynamics function dependent on u2.")
     end
     DiscreteLinearModel(h, A, B, C, d)
@@ -194,12 +194,12 @@ function DiscreteLinearModel(model::LinearModel, h)
     if norm(getaffinecoeffs(model)) > 0
         throw(ArgumentError("Cannot use exponential integrator for an integrator with an affine term."))
     end
-    n,m = RD.dims(model)
+    n, m = RD.dims(model)
     A = getstatecoeffs(model)
     B = getcontrolcoeffs(model)
-    expAB = exp([A*h B*h; zeros(m,n+m)])
-    A = expAB[1:n,1:n]
-    B = expAB[1:n,n+1:n+m]
+    expAB = exp([A*h B*h; zeros(m, n + m)])
+    A = expAB[1:n, 1:n]
+    B = expAB[1:n, n+1:n+m]
     DiscreteLinearModel(h, A, B)
 end
 
@@ -212,6 +212,10 @@ getstatecoeffs(model::DiscreteLinearModel) = model.A
 getcontrolcoeffs(model::DiscreteLinearModel) = model.B
 getnextstatecoeffs(model::DiscreteLinearModel) = model.C
 getaffinecoeffs(model::DiscreteLinearModel) = model.d
+
+function Base.copy(model::DiscreteLinearModel)
+    DiscreteLinearModel(model.dt, model.A, model.B, model.C, model.d)
+end
 
 isstable(model::DiscreteLinearModel) = RandomLinearModels.isstabled(getstatecoeffs(model))
 
@@ -232,30 +236,30 @@ function RD.discrete_dynamics!(model::DiscreteLinearModel, xn, x, u, t, h)
     checktimestep(model, h)
     xn .= model.Cd
     mul!(xn, model.CA, x, true, true)
-    mul!(xn, model.CB, u, true, true) 
-    nothing 
-end
-
-function RD.jacobian!(model::DiscreteLinearModel, J, xn, x, u, t, h)
-    checktimestep(model, h) 
-    n,m = RD.dims(model)
-    J[:,1:n] .= model.CA
-    J[:,n+1:n+m] .= model.CB
+    mul!(xn, model.CB, u, true, true)
     nothing
 end
 
-function RD.dynamics_error(model::DiscreteLinearModel, 
-                           z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
+function RD.jacobian!(model::DiscreteLinearModel, J, xn, x, u, t, h)
+    checktimestep(model, h)
+    n, m = RD.dims(model)
+    J[:, 1:n] .= model.CA
+    J[:, n+1:n+m] .= model.CB
+    nothing
+end
+
+function RD.dynamics_error(model::DiscreteLinearModel,
+    z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
     checktimestep(model, RD.timestep(z1))
-    x1,u1 = RD.state(z1), RD.control(z1)
+    x1, u1 = RD.state(z1), RD.control(z1)
     x2 = RD.state(z1)
     model.A * x1 + model.B * u1 + model.C * x2 + model.d
 end
 
-function RD.dynamics_error!(model::DiscreteLinearModel, y2, y1, 
-                            z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
+function RD.dynamics_error!(model::DiscreteLinearModel, y2, y1,
+    z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
     checktimestep(model, RD.timestep(z1))
-    x1,u1 = RD.state(z1), RD.control(z1)
+    x1, u1 = RD.state(z1), RD.control(z1)
     x2 = RD.state(z1)
     y2 .= model.d
     mul!(y2, model.A, x1, true, true)
@@ -264,12 +268,12 @@ function RD.dynamics_error!(model::DiscreteLinearModel, y2, y1,
     nothing
 end
 
-function RD.dynamics_error_jacobian!(model::DiscreteLinearModel, J2, J1, y2, y1, 
-                                     z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
+function RD.dynamics_error_jacobian!(model::DiscreteLinearModel, J2, J1, y2, y1,
+    z2::RD.AbstractKnotPoint, z1::RD.AbstractKnotPoint)
     checktimestep(model, RD.timestep(z1))
-    n,m = RD.dims(model)
-    J2[:,1:n] .= model.C
-    J1[:,1:n] .= model.A
-    J1[:,n+1:n+m] .= model.B
+    n, m = RD.dims(model)
+    J2[:, 1:n] .= model.C
+    J1[:, 1:n] .= model.A
+    J1[:, n+1:n+m] .= model.B
     nothing
 end
