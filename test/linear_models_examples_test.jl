a3abc6eddc928f71a912680b9975d20be874745d
Brian Jackson <bjack205@gmail.com>
2024-03-21 14:45:31 -0400

    Upgrade rotations (#15)
    * Upgrade Rotations
    
    * Update formatting
    
    * Don't skip version
    
    ---------
    
    Co-authored-by: Brian Jackson <brian@albedo.com>

--- test/linear_models_examples_test.jl
+++ test/linear_models_examples_test.jl
@@ -6,22 +6,23 @@ using FiniteDiff
 using LinearAlgebra
 using Test
 using Random
-Random.seed!(1)
+
+Random.seed!(2)
 const RD = RobotDynamics
 
 ## Double Integrator
 D = 2
 linmodel = RobotZoo.LinearModels.DoubleIntegrator(D)
 model = RobotZoo.DoubleIntegrator(D)
-x,u = rand(model)
+x, u = rand(model)
 @test RD.dynamics(model, x, u) ≈ RD.dynamics(linmodel, x, u)
 
 h = 0.1
 dmodel_exp = RobotZoo.DiscreteLinearModel(linmodel, h)
 dmodel_lin = RobotZoo.LinearModels.DiscreteDoubleIntegrator(h, D)
-@test RD.discrete_dynamics(dmodel_exp, x, u, 0.0, h) ≈ 
-    RD.discrete_dynamics(dmodel_lin, x, u, 0.0, h)
+@test RD.discrete_dynamics(dmodel_exp, x, u, 0.0, h) ≈
+      RD.discrete_dynamics(dmodel_lin, x, u, 0.0, h)
 
 ## FlexibleSatellite
 model = RobotZoo.LinearModels.FlexibleSatellite()
-@test RD.dims(model) == (12,3,12)
+@test RD.dims(model) == (12, 3, 12)
